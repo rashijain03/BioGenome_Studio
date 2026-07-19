62756f7be16b635fbc8734abe61526a32601f738
Rashi Jain <rashi.jain_bt.bio25@gla.ac.in>
2026-07-19 15:40:25 +0530

    Added BioGenome Pipeline
    Created a menu-driven pipeline launcher for BioGenome Studio. The pipeline uses subprocess to run all 9 bioinformatics projects from a single interface and includes improved execution handling using sys.executable.

---
+++ BioGenome_Pipeline.py
@@ -0,0 +1,64 @@
+import subprocess
+import sys
+
+print("=" * 45)
+print("        BioGenome Pipeline")
+print("=" * 45)
+
+while True:
+
+    print("\n1. Genome Quality Analyser")
+    print("2. FASTA File Explorer")
+    print("3. DNA RNA Converter")
+    print("4. Protein Translator")
+    print("5. ORF Finder")
+    print("6. Mutation Variant Detector")
+    print("7. DNA Motif Finder")
+    print("8. K-mer Analyzer")
+    print("9. Genome Visualization Dashboard")
+    print("0. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "0":
+        print("\nExiting BioGenome Pipeline...")
+        break
+
+    elif choice == "1":
+        print("\nLaunching Genome Quality Analyzer...")
+        subprocess.run([sys.executable, "01_Genome_Quality_Analyser.py"])
+
+    elif choice == "2":
+        print("\nLaunching FASTA File Explorer...")
+        subprocess.run([sys.executable, "02_FASTA_File_Explorer.py"])
+
+    elif choice == "3":
+        print("\nLaunching DNA RNA Converter...")
+        subprocess.run([sys.executable, "03_DNA_RNA_Converter.py"])
+
+    elif choice == "4":
+        print("\nLaunching Protein Translator...")
+        subprocess.run([sys.executable, "04_Protein_Translator.py"])
+
+    elif choice == "5":
+        print("\nLaunching ORF Finder...")
+        subprocess.run([sys.executable, "05_ORF_Finder.py"])
+
+    elif choice == "6":
+        print("\nLaunching Mutation Variant Detector...")
+        subprocess.run([sys.executable, "06_Mutation_Variant_Detector.py"])
+
+    elif choice == "7":
+        print("\nLaunching DNA Motif Finder...")
+        subprocess.run([sys.executable, "07_DNA_Motif_Finder.py"])
+
+    elif choice == "8":
+        print("\nLaunching K-mer Analyzer...")
+        subprocess.run([sys.executable, "08_Kmer_Analyzer.py"])
+
+    elif choice == "9":
+        print("\nLaunching Genome Visualization Dashboard...")
+        subprocess.run([sys.executable, "09_Genome_Visualization_Dashboard.py"])
+
+    else:
+        print("\nInvalid choice! Please enter a number between 0-9.")
